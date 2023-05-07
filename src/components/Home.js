9deb191244fead6879974496075c2884b4275239
Paladyn <95892157+RobbChunn@users.noreply.github.com>
2023-05-07 16:57:10 -0500

    changed title to not look like shit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,28 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 
-// const DuosButton = styled.button`
-//   display: flex;
-//   flex-direction: row;
-//   align-items: center;
-//   width: 200px;
-//   margin-left: 400px;
-//   margin-right: 40px;
-//   margin-top: 30px;
-//   gap: 1rem;
-//   padding: 15px 32px;
-//   text-align: center;
-//   text-decoration: none;
-//   display: inline-block;
-//   font-size: 16px;
-
-//   &:hover {
-//     background-color: #d50100;
-//     cursor: pointer;
-//   }
-// `;
-
-
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -45,7 +23,8 @@ const DuosButton = styled.button`
   text-decoration: none;
   display: inline-block;
   font-size: 16px;
-  border-radius: 8px; /* Add rounded edges */
+  font-weight: bold;
+  border-radius: 16px; /* Add rounded edges */
   background-color: #fafafa; /* Add desired background color */
   color: #6585cf; /* Add desired text color */
 
@@ -70,7 +49,8 @@ const TriosButton = styled.button`
   text-decoration: none;
   display: inline-block;
   font-size: 16px;
-  border-radius: 8px; /* Add rounded edges */
+  font-weight: bold;
+  border-radius: 16px; /* Add rounded edges */
   background-color: #fafafa; /* Add desired background color */
   color: #6585cf; /* Add desired text color */
 
@@ -81,6 +61,23 @@ const TriosButton = styled.button`
   }
 `;
 
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: #fafafa;
+  width: 36em;
+  margin: 0 auto;
+  margin-top: 1em;
+  border-radius: 24px;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  color: #d50100;
+  padding: 16px 32px;
+  border-radius: 16px;
+  `;
+
 
 // flex items-center justify-center text-sm px-8 py-4 font-bold transition bg-green-100 border-4 
 // border-black rounded-xl focus:outline-none 
@@ -89,7 +86,9 @@ const TriosButton = styled.button`
 const Home = () => {
   return (
     <div>
-      <h1>Choose your Game Mode!</h1>
+      <TitleContainer>
+      <Title>Choose your Game Mode!</Title>
+      </TitleContainer>
       <ButtonContainer>
         <Link to="/Duos">
           <DuosButton className="button">Duos!</DuosButton>
